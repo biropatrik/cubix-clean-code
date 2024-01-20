f6abc7709222683e69fc39a813f2856f1b4f4fb7
biropatrik <biropatrik9612@gmail.com>
2024-01-20 12:22:25 +0100

    [Feature] gradebook refactored

--- src/gradebook.ts
+++ src/gradebook.ts
@@ -1,18 +1,27 @@
 export class Gradebook {
 
+    readonly DEFAULT_GRADE = 'D';
+    readonly GRADES = new Map<number, string>([
+        [70, 'C'],
+        [80, 'B'],
+        [90, 'A']
+    ])
+
     public calculateGrade(score: number): string {
-        if (score >= 90) {
-            return 'A';
-        } else {
-            if (score >= 80) {
-                return 'B';
-            } else {
-                if (score >= 70) {
-                    return 'C';
-                } else {
-                    return 'D';
-                }
-            }
+        return this.getGradeFromMapByScore(this.GRADES, score);
+    }
+
+    private getGradeFromMapByScore(grades: Map<number, string>, score: number) {
+        let result: string = this.DEFAULT_GRADE;
+
+        for (let key of grades.keys()) {
+            result = this.isAGreaterThenEqualB(score, key) ? grades.get(key)! : result;
         }
+
+        return result;
+    }
+
+    private isAGreaterThenEqualB(a: number, b: number) {
+        return a >= b;
     }
 }
--- test/gradebook.test.ts
+++ test/gradebook.test.ts
@@ -10,11 +10,11 @@ describe('Gradebook tests', () => {
     it.each`
     testName | score | expectedGrade
     ${'A grade when score > 90'} | ${91} | ${'A'}
-    ${'A grade when score => 90'} | ${90} | ${'A'}
+    ${'A grade when score >= 90'} | ${90} | ${'A'}
     ${'B grade when score > 80'} | ${89} | ${'B'}
-    ${'B grade when score => 80'} | ${80} | ${'B'}
+    ${'B grade when score >= 80'} | ${80} | ${'B'}
     ${'C grade when score > 70'} | ${75} | ${'C'}
-    ${'C grade when score => 70'} | ${70} | ${'C'}
+    ${'C grade when score >= 70'} | ${70} | ${'C'}
     ${'D grade when score < 70'} | ${50} | ${'D'}
     `('should return the $testName', ({score, expectedGrade}) => {
         // Act
