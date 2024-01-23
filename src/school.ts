66c1981c9c1b6490b4527c6e2c4bc0b524531274
biropatrik <biropatrik9612@gmail.com>
2024-01-23 21:15:55 +0100

    [Feature] school refactored

---
+++ src/school.ts
@@ -0,0 +1,44 @@
+class School {
+    private classes: SchoolClass[] = [];
+
+    constructor() { }
+
+    addClass(schoolClass: SchoolClass): void {
+        this.classes.push(schoolClass);
+    }
+
+    getStudentCount(): number {
+        let totalStudents = 0;
+
+        for (const schoolClass of this.classes) {
+            totalStudents += schoolClass.getStudentCount();
+        }
+        return totalStudents;
+    }
+}
+
+class SchoolClass {
+    private students: Student[] = [];
+
+    constructor() { }
+
+    addStudent(student: Student): void {
+        this.students.push(student);
+    }
+
+    getStudents(): Student[] {
+        return this.students;
+    }
+
+    getStudentCount(): number {
+        return this.students.length;
+    }
+}
+
+class Student {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
